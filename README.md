e921192204099f5a1f26903b02c10ca852e3fef5
ShmidtAlex <shmidt.alex@gmail.com>
2021-01-20 18:24:07 +0300

    npprogress + axios interceptors added

--- README.md
+++ README.md
@@ -18,20 +18,28 @@ npm run build
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
+## Install json-server
+
+  npm install -g json-server
+
 ### Run json-server
 
-	json-server --watch db.json
+  json-server --watch db.json
+
+### Delay imitation on json-server
+  
+  json-server -d { delay millisecons } db.json
 
 ## Content:
-	
-	Vuex organization
-	Routing
-	Pagination
-	Progress bar with 3 ways: a) axios interceptors
-							  b) in-component Route Guards
-							  c) global and Per-Route Guards
+  
+  Vuex organization
+  Routing
+  Pagination
+  Progress bar with 3 ways: a) axios interceptors
+                b) in-component Route Guards
+                c) global and Per-Route Guards
 
 ## Extra tasks (not implemented yet):
 
-	caching for routing during pagination
-	implement vuex namespasing
+  caching for routing during pagination
+  implement vuex namespasing
--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import  NProgress from 'nprogress'
+
 const apiClient = axios.create({
   baseURL: `http://localhost:3000`,
   withCredentials:  false,
@@ -7,7 +9,26 @@ const apiClient = axios.create({
     'Content-Type': 'application/json',
   }
 })
+//for simulating delay use terminal command "json-server -d 1500 db.json"
+apiClient.interceptors.request.use(config => {
+  NProgress.start()
+  return config
+})
+apiClient.interceptors.response.use(response => {
+  NProgress.done()
+  return response
+})
 
+/*Caviats of this (axios interceptors + NProgress library) approach: 
+  1. Not appropriate whith multiple API calls at the same time, because will show the progress
+    of all three calls, but not one. (It can be resolved with a Loading Vuex Module watch lesson 
+    2, 5:54)
+  2. The templates get loaded before API calls is returned. And they look empty, 
+    which is not good, it's not optimal user experience.
+Pros of the approach:
+  1. Interceptors allows to set authorization tokens
+  2. On response to format or filter data before it reaches into the app
+*/
 export default { 
   getEvents(perPage, currentPage) {
     return apiClient.get(`/events?_limit=${ perPage }&_page=${currentPage}`)
